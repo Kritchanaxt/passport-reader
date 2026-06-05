254d466253abd297d28857580262fc7c0ffe2f39
Kritchanaxt <devid007x@gmail.com>
2026-06-05 11:06:51 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 28
         targetSdk = 36
         versionCode = 23
-        versionName = "3.5"
+        versionName = "3.6"
         ndk {
             abiFilters.addAll(setOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64"))
         }
